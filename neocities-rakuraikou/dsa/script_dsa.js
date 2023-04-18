cc8e362a1541ef6f6e5080da4e707f38cd5ccf7a
THUNDER <lenicarusdo@gmail.com>
2023-04-17 20:44:17 -0400

    dsa page

---
+++ neocities-rakuraikou/dsa/script_dsa.js
@@ -0,0 +1,51 @@
+function insertHeader() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-header-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("include-header-html");
+          insertHeader();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+function islaStatic() {
+  var isla = document.getElementById("isla");
+  isla.addEventListener('mouseenter', event => {
+    isla.src = isla.src.replace(/\.png$/, '.gif')
+  })
+  isla.addEventListener('mouseleave', event => {
+    isla.src = isla.src.replace(/\.gif$/, '.png')
+  })
+}
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
