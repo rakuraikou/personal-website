b52d922a49953297ae99ebbcdabc0d91d447bb19
THUNDER <lenicarusdo@gmail.com>
2024-02-29 21:10:12 -0500

    the php migration Begins

---
+++ bftb/wiki/lore/magic.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>the thunder guy or whatever</title>
+
+<!-- discord embed -->
+    <meta content="the thunder guy or whatever" property="og:site_name">
+    <meta content="Bolt from the Blue - Index" property="og:title">
+    <meta content="Lore and development site for LEN's worldbuilding and RPG project, Bolt from the Blue." property="og:description">
+    <meta content='https://rkrk.dev/bftb/wiki/res/icon.png' property='og:image'>
+    <meta name="theme-color" content="#34c3ff">
+
+<!-- css sheet and javascript -->
+    <link href="/bftb/wiki/style_wiki.css" rel="stylesheet" type="text/css" media="all">
+    <script src="/bftb/wiki/script_wiki.js"></script>
+
+  </head>
+  <body>
+    <?php include '../header_content.html'; ?>
+    <main>
+      <ul class="crumb">
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="index.php">Lore</a></li>
+        <li><a href="magic.php">Magic</a></li>
+      </ul>
+      <h1>
+        Magic
+      </h1>
+      <hr>
+      <h2>
+        Light
+      </h2>
+      <hr>
+      <p>
+        Everything on Luxos is made of "light," a compound and force colloquially referred to as "life force" or "magical energy." Light is not only present in every entity and lifeform, but it also flows in a "pure" state in the atmosphere. All sentient beings can manipulate one type of light through the favor of a Beastlord. This "type" is known as an aspect.
+      </p>
+      <h2>
+        Aspects
+      </h2>
+      <hr>
+      <p>
+        Aspects are defined as the specific type of light a person (or other sentient being) can conjure or manipulate. For example, someone with a water aspect is able to transmute pure light into water and control it. This person could also control water that is already present, such as from rivers and oceans. Aspects are obtained from one of the four Beastlords, and each aspect is specific to a certain Beastlord. Generally decided at the moment of birth, the reasons one receives a particular Beastlord's favor are rather arbitrary. The majority of Luxos' population are only able to use one aspect. Certain individuals known as keepers are born with special bonds to the Beastlords, and they are able to use up to three depending on the Beastlord that they are linked to.
+      </p>
+      <p>
+        Aspects are powerful and versatile, and society has explored many applications and techniques over time. However, despite its great potential, there is a limit to one's ability to use an aspect: as one uses it, a dangerous waste byproduct called "cael" is collected in the bloodstream where light once flowed. High cael buildup can lead to serious consequences (and ultimately death) if left untreated or unaccounted for.
+      </p>
+      <h2>
+        Cael
+      </h2>
+      <hr>
+      <p>
+        Cael is a substance that manifests in the bloodstream in the place of light when magic is used. Like light, the composition of cael is not entirely underestood. Low cael buildup manifests as exhaustion, fatigue and muscle aches. Though cael within the body will dispel on its own eventually, rest and hydration will quicken the recovery process. Some people dispel cael more quickly than others due to a biological predisposition (such as a higher metabolism).
+      </p>
+      <p class="notebox">
+        It's also been noted that spending time around other people, particularly loved ones, increases the rate at which someone recovers. Though concrete explanations are yet to be found, scholars speculate that this is due to a community survival instinct present in which people subconsciously "share" their light with each other.
+      </p>
+      <p>
+        In higher accumulations, cael may cause an individual suffer a condition known as caelism. Common tells for caelism include weakened or impaired bodily functions (such as difficulty breathing or slower injury recovery) and a telltale "bruising" or "blighting" across various parts of the body depending on the specific aspect. Caelism takes a significant amount of time to run its complete course before it is dispelled, and scarring from the blighting may remain even after a full recovery. Medicine exists to increase the rate at which cael is dispelled, but it's better to just never let things get to this point in the first place.
+      </p>
+      <p>
+        Though caelism is very dangerous, prognosis is rarely ever certain death. Caelism initially presents itself in a localized area - if aggravated, it will travel to the heart, where it will likely become fatal. Once symptoms are recognized, it is recommended that anyone with caelism find immediate medical care and rest accordingly. Of course, it's also considered common sense to avoid using more magic at all costs once caelism manifests.
+      </p>
+    </main>
+    <?php include '../footer_content.html'; ?>
+
+  </body>
+</html>
