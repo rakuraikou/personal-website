3a7384b3120a7f9ff70a8960c7e3142b8c9afade
THUNDER <lenicarusdo@gmail.com>
2024-03-05 15:53:51 -0500

    the unphpification

--- bftb/wiki/character.php
+++ bftb/wiki/character.php
@@ -154,19 +154,7 @@
                 echo "Invalid character ID!";
             }
         } else {
-          //include 'character_index.html';
-          while ($row = $result->fetch_assoc()) {
-            echo "
-            <h1>Characters</h1>
-            <hr>
-            <h2 style='text-align: center'>Main Characters</h2>
-            <div class='char-select'>
-              <a href='?id=" . $row["id"] . "'>" . $row["name"] . "
-              <img src='res/200.jpg'>
-              </a>
-            </div>
-            ";
-          }
+          include 'character_index.html';
         }
         $conn->close();
         ?>
