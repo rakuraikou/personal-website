30e5689f6c16046f3a6186e2eb6ce8fea8c49104
THUNDER <lenicarusdo@gmail.com>
2024-03-07 16:10:16 -0500

    faq

---
+++ bftb/faq.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>the thunder guy or whatever</title>
+
+<!-- discord embed -->
+    <meta content="the thunder guy or whatever" property="og:site_name">
+    <meta content="Bolt from the Blue - FAQ" property="og:title">
+    <meta content="Lore and development site for LEN's worldbuilding and RPG project, Bolt from the Blue." property="og:description">
+    <meta content='https://rkrk.dev/bftb/wiki/res/icon.png' property='og:image'>
+    <meta name="theme-color" content="#23c4ff">
+
+<!-- css sheet and javascript -->
+    <link href="style_bftb.css" rel="stylesheet" type="text/css" media="all">
+    <script src="script_bftb.js"></script>
+
+  </head>
+  <body>
+    <?php include 'header_content.html'; ?>
+    <main>
+      <h1>
+        Frequently Asked Questions
+      </h1>
+      <b>Q. When is the game getting released? / When is the demo?</b>
+      <p class="blockquote">
+        A. Whenever it is released. In all seriousness, BFTB does not have a solid release date currently. My goal is to have a polished demo up to the end the game's first chapter by February 2025 in time for Steam Next Fest, but I am really hoping to have a playable feature test / alpha much, much sooner!
+      </p>
+      <b>Q. What engine is BFTB being made in?</b>
+      <p class="blockquote">
+        Godot 4. Really went back and forth on quite a few engines before settling on this one. I just like their style.
+      </p>
+      <b>Q. How can I help?</b>
+      <p class="blockquote">
+        A. I'm not looking for any kind of financial support at the moment, nor am I bringing any new team members on. This is subject to change as the project starts to take shape, but for now just keep me and my darling project in your heart!... and maybe give me a follow on <a href="/abt">some social medias</a> if you'd like.
+      </p>
+    </main>
+    <?php include 'footer_content.html'; ?>
+  </body>
+</html>
