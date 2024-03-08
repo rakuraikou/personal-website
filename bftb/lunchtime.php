96ab44946d2241960e3efa1693ca4fc02b973cc1
THUNDER <lenicarusdo@gmail.com>
2024-03-08 16:49:54 -0500

    data- please

--- bftb/lunchtime.php
+++ bftb/lunchtime.php
@@ -40,9 +40,7 @@
         if ($result->num_rows > 0) {
           echo "<div class='gallery'>";
           while ($row = $result->fetch_assoc()) {
-            echo "
-            <img class='gallery-img thumbnail' src='res/lunchtime/thumb/" . $row["artist"] . "/" . $row["thumbnail"] . "' alt='" . $row["thumbnail"] . " thumbnail'>
-            ";
+            echo "<img class='gallery-img thumbnail' src='res/lunchtime/thumb/" . $row["artist"] . "/" . $row["thumbnail"] . "' alt='" . $row["thumbnail"] . " thumbnail' data-full='" . $row["full"] . "' data-alt='" . $row["alt"] . "' data-artist-link='" . $row["artist-link"] . "' data-artist-name='" . $row["artist-name"] . "' data-caption='" . $row["caption"] . "'>";
           }
           echo "</div>";
         } else {
@@ -73,7 +71,7 @@
               &lt;
             </span>
           </span>
-          <img id='modal-image' src='' alt='" . $row["alt"] . "'>
+          <img id='modal-image' src='' alt=''>
           <span id='next-btn' class='text-outline modal-btn'>
             <span>
               &gt;
@@ -82,12 +80,11 @@
         </div>
         <div class='modal-caption'>
           <div class='modal-credit'>
-            <span>by <a href='" . $row["artist_link"] . "'>" . $row["artist_name"] . "</a></span>
-            <a href='" . $row["full"] . "'>View full image</a>
+            <span>by <a id='credit' href=''></a></span>
+            <a id='view-full' href=''>View full image</a>
           </div>
           <hr style='width: 100%'>
-          <span>
-          " . $row["caption"] . "
+          <span id='caption'>
           </span>
         </div>
       </div>
@@ -98,6 +95,7 @@
       <script>
         const modal = document.getElementById('lt-modal');
         const modalImage = document.getElementById('modal-image');
+        const artistCredit = document.getElementById('credit');
         const thumbnails = document.querySelectorAll('.thumbnail');
         const prevBtn = document.getElementById('prev-btn');
         const nextBtn = document.getElementById('next-btn');
@@ -106,7 +104,13 @@
         thumbnails.forEach(thumbnail => {
           thumbnail.addEventListener('click', () => {
             modal.style.display = 'flex';
-            //modalImage.src = thumbnail.dataset.full;
+            modalImage.src = thumbnail.dataset.full;
+            modalImage.alt = thumbnail.dataset.alt;
+            artistCredit.href = thumbnail.dataset.artist_link;
+            artistCredit.innerText = thumbnail.dataset.artist_name;
+            document.getElementById('caption').innerText = thumbnail.dataset.caption;
+            document.getElementById('view-full').href = thumbnail.dataset.full;
+
           });
         });
 
