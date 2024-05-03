49f495582741919f8519ef2a57e879235866a0e3
THUNDER <lenicarusdo@gmail.com>
2024-05-03 15:47:00 -0400

    bravely marle real

---
+++ bravelymarle.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>the thunder guy or whatever</title>
+
+<!-- discord embed -->
+    <meta content="the thunder guy or whatever" property="og:site_name">
+    <meta content="THUNDERSWAG!!! - Bravely Marle!" property="og:title">
+    <meta content="LEN's personal site for oc/worldbuilding, gamedev, hobbies, and other self-indulgent stuff." property="og:description">
+    <meta content='https://rkrk.dev/res/embed_sprite.png' property='og:image'>
+    <meta name="theme-color" content="#ffa114">
+
+<!-- css sheet and javascript -->
+
+    <link href="style.css" rel="stylesheet" type="text/css" media="all">
+    <script src="script.js"></script>
+
+  </head>
+
+  <body>
+    <?php include 'header_content.html'; ?>
+    <main>
+      <h2>BRAVELY MARLE</h2>
+      <p>
+        "Bravely Marle" is the sorta-placeholder name for my small party of OCs that exist in the Bravely Default (Luxendarc) universe! It's not super developed, but the concepts I've written out so far make me pretty happy [: If you'd like to see more of these characters, I have a <a href="https://toyhou.se/charge/characters/folder:5099810" target="_blank">TOYHOU.SE folder</a> with their profiles and galleries.
+      </p>
+      <div class="grid-bravelymarle bm-1">
+        <p class="bm-1-txt">
+          Marle is an angler and fishmonger living on a remote island in the Sea of Corsairs between Caldis and Florem. She is the bearer of the fisherman asterisk. She's not terribly familiar with the affairs of the mainland, relying on travelers to pass any juicy details her way. One day, she hears about Adas Idalia, an otherworldly pool of water in the "eye" of the isle of Rudiment.<span style="color: #ffc94d">*</span> Marle falls in love with the idea of catching fish in this pool. So, with her best friend Sorrell, she aspires to form her own seafaring crew and sail across Luxendarc in search of Rudiment!
+        </p>
+        <img class="bm-1-img" src="res/marle/marle_banner.png" alt="Marle banner" style="width: 100%; border-radius: 10px;">
+      </div>
+      <p>
+        Ironically, she acquired the fisherman asterisk by complete accident—it was caught in a net between her other, fishier catches of the day. Mechanically, the fisherman asterisk's job ability is called "Casting." the job has proficiency in spears/daggers and water-imbued attacks. (NOTE: I'd love to dev out the job more...but that's a task for another day.)
+      </p>
+      <div class="grid-bravelymarle bm-2">
+        <img src="res/marle/marle_and_sorrell.png" alt="Marle and Sorrell" style="width: 100%; border-radius: 10px;">
+        <div>
+          <p>
+            Sorrell is Marle's best friend. They grew up together, but parted ways later in life when Sorrell began studying at the prestigious Gaius Arcana<span style="color: #ffc94d">*</span>. Unlike Marle, Sorrell doesn't have an asterisk, but his intelligence and natural talent for the magical arts caught the attention of the baron, who personally recruited him. After about a year of study, Sorrell realized that he didn't really care for academia and found little personal value in staying at the institution. When Marle asked him to join her on her travels, he took it as the perfect opportunity to drop out and do something "interesting" for once.
+          </p>
+          <p>
+            Compared to Marle, Sorrell is much more reserved. He speaks in quiet mumbles and keeps his feelings to himself; this makes him seem more mature and astute, but the truth is he's just kinda shy. The hair over Sorrell's left eye is naturally bleached, and the eye underneath is damaged/scarred. This dramatic injury was due to him mistakenly walking into another Arcana student's project at an inopportune time. Surprisingly, this event has little bearing on his personality, inner traumas, or reason for ultimately leaving the Gaius Arcana.
+          </p>
+        </div>
+        <div>
+          <p style="margin-top: 0">
+            Though they couldn't be more different, Marle and Sorrell understand the value in having each other, and consider the other to be their own best friend. Where Marle struggles to find a path forward, Sorrell brilliantly maps out a plan of action. When Sorrell hesitates to take a leap of faith, Marle eagerly leads the way.
+          </p>
+          <p>
+            The journey is bigger than the two of them, however, and they'll need to bring on more allies before they can take on Rudiment...I'll get to the rest of this eventually lol. Sorry haha
+          </p>
+        </div>
+        <a href="https://twitter.com/violetchio" target="_blank" title="art by violetchio on Twitter">
+          <div style="display: grid; grid-template-columns: 59% 41%; align-items: center;">
+            <img src="res/marle/ango_marle.png" alt="Marle by violetchio" style="width: 100%">
+            <img src="res/marle/ango_sorrell.png" alt="Sorrell by violetchio" style="width: 100%">
+          </div>
+        </a>
+      </div>
+      <p>
+        <span style="color: #ffc94d">*</span>Rudiment, Adas Idalia and the Gaius Arcana are Bravely worldbuilding concepts by my friend <a href="https://twitter.com/chuunicherry" target="_blank">Fio</a>! love u dude 
+      </p>
+    </main>
+    <?php include 'footer_content.html'; ?>
+  </body>
+</html>
