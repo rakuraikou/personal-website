41e7a3bc492592d3500efb5c9b2eb968bdb33671
THUNDER <lenicarusdo@gmail.com>
2024-06-02 11:56:35 -0400

    rewrite

--- bftb/wiki/lore/beastlords.php
+++ bftb/wiki/lore/beastlords.php
@@ -30,47 +30,33 @@
       </h1>
       <hr>
       <p>
-        Four unfathomably magnificent creatures "rule" Luxos' natural world, also known as the Beastlords of Storm, Tide, Ash and Wild. Each Beastlord is known to gift one of three <a href="magic">aspects</a> to an individual:
-      </p>
-      <ul>
-        <li>
-          <b>Storm</b> : wind, lightning, sun
-        </li>
-        <li>
-          <b>Tide</b> : water, ice, moon
-        </li>
-        <li>
-          <b>Ash</b> : fire, crystal, dust
-        </li>
-        <li>
-          <b>Wild</b> : earth, flora, toxin
-        </li>
-      </ul>
-      <p class="notebox">
-        In addition to the four known Beastlords, there have been repots of a fifth Beastlord that's rested dormant within the core of Luxos since the world's creation. Governing the very light that unites all entities, the aptly-named Beastlord of Light is said to be the source of the Azure Chasm's opening 10 years ago.
+        Beastlords are defined by the "aura" (or "pressure") they exert onto the light surrounding them. Everything in the realm was forged and continues to be altered by the shifts in light that take place as a result of their existence.
       </p>
       <p>
-        Luxos was more or less created by these beings. Everything on Luxos - animals, plants, soil, and more - were forged with influence by the Beastlords, and they all have some level of light from these Beastlords embedded into them. This influence is what allows the sentient life on Luxos to utilize an aspect.
+        The three Beastlords are Boreas of Storm, Mizuchi of Tide, and Chimera of Ash.
       </p>
       <h2>
         Behavior
       </h2>
       <hr>
       <p>
-        The Beastlords themselves move from place to place in somewhat predictable manners, frequently returning to their dens. Though they can generally be found in these dens, it's quite dangerous to enter one. Beastlords constantly emit an aura ("pressure") that shifts the atmospheric light around them, making it strenuous and difficult for anyone to get near. It is this pressure that also shapes the world and its natural phenomena, such as weather, nutrient cycling and the flow of atmospheric light.
+        Beastlords move from place to place in somewhat predictable manners, frequently returning to their dens. Though they can generally be found in these dens, it's quite dangerous to enter one—the Beastlord's pressure makes it strenuous and difficult for anyone to get near. It is this pressure that also shapes the world and its natural phenomena, such as weather, nutrient cycling and the flow of atmospheric light.
+      </p>
+      <p class="notebox">
+        Luxos' regions are named after Beastlord dens. In general, cultural identity is pretty strongly shaped by the regional Beastlord. The Beastlords really do have a hand in everything around them!
       </p>
       <p>
-        Beastlords are closer to animals than people in terms of behavior, acting on habit or instinct rather than complex logical deduction. They don't seem to be influenced or particularly concerned with the affairs of people or other beings, save for the <a href="keepers">keepers</a> connected to them. Likewise, most people don't worship the Beastlords, instead reserving their reverence for the Champions. That being said, people generally know not to "cross" one, both literally and figuratively, as they are highly powerful beings that don't necessarily listen to reason.
+        Beastlords are closer to animals than people in terms of behavior, acting on habit or instinct rather than complex logical deduction. They don't seem to be influenced or particularly concerned with the affairs of people or other beings. Likewise, most people don't worship the Beastlords, instead reserving their reverence for the Champions. That being said, people generally know not to "cross" a Beastlord, both literally and figuratively, as they are highly powerful beings that don't necessarily listen to reason.
       </p>
       <h2>
         Talismans
       </h2>
       <hr>
       <p>
-        The physical form of the Beastlord itself holds immense power. Tufts of fur, scales, claws, and other trace materials regularly shed from the Beastlords can increase the capacity of a given person's aspect in various ways just by carrying one around as an accessory (aka a talisman).
+        The physical form of the Beastlord itself holds immense power. Tufts of fur, scales, claws, and other trace materials regularly shed from the Beastlords can increase the capacity of a given person's aspect in magnificent ways just by carrying one around as an accessory. These accessories are generally referred to as "talismans."
       </p>
       <p>
-        Because of how hard it is to even get close to a Beastlord, talisman materials are extremely rare to come by. Additionally, the pressure exerted by a Beastlord causes shed materials to disintegrate rather quickly after falling off. Someone lucky enough to stumble upon one would need to act fast, as it's very likely that the Beastlord it came from is nearby (and therefore warping the light around it). It's no surprise that talismans are closely associated with authority or nobility - the very few people who have a talisman also happen to be rulers or high-ranking military personnel of some sort.
+        Because of how hard it is to even get close to a Beastlord, talisman materials are extremely rare to come by. Additionally, the pressure exerted by a Beastlord causes shed materials to disintegrate rather quickly after falling off. Someone lucky enough to stumble upon one would need to act fast, as it's very likely that the Beastlord it came from is nearby (and therefore warping the light around it). It's no surprise that talismans are closely associated with authority or nobility—the very few people who have a talisman also happen to be rulers or high-ranking military personnel of some sort.
       </p>
     </main>
     <?php include '../footer_content.html'; ?>
