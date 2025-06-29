785d27e18a08522fc64c57c9a73b730ee4fedb4a
THUNDER <lenicarusdo@gmail.com>
2025-06-28 23:57:12 -0400

    updating boltysite with new lore n art

--- bftb/story.php
+++ bftb/story.php
@@ -27,10 +27,10 @@
         <img src="res/ango_lt.png" style="float: right; max-height: 250px" alt="Raiden comforting Araceli">
       </a>
       <p>
-        Embark on an epic journey where the strength of one's bonds has the power to overcome even prophecy. <i>Bolt from the Blue</i> is a character-driven RPG following the story of Araceli Almador de Luna, princess of a kingdom that's just been brought to ruin by a powerful, dark entity - one that she was destined to stop, but couldn't due to a mysterious curse that's plagued her for her entire life. Araceli's story is shown through the perspective of Raiden, her kind attendant with no recollection of her own past, devoting herself entirely to Araceli's safety and well-being.
+        Embark on an epic journey where the strength of one's bonds has the power to overcome even prophecy. <i>Bolt from the Blue</i> is a character-driven RPG following the story of Araceli Almador de Luna, princess of a kingdom that's just been brought to ruin by a powerful, dark entity—one that she was destined to stop, but couldn't due to a mysterious curse that's plagued her for her entire life. Araceli's story is shown through the perspective of Raiden, her kind attendant with no recollection of her own past, devoting herself entirely to Araceli's safety and well-being.
       </p>
       <p>
-        Accompanied by over-ambitious noble Saffron and the notorious solo pirate Cole, the duo set out on a quest to lift Araceli's curse and return home and fulfill her destiny as her kingdom's hero. The party is threatened at every turn by the perpetrators of the destruction: Nox Noctis, a mysterious organization seeking to control the Beastlords. Together, these four will put a stop to Nox Noctis' plans, uncover ancient secrets, and bring light to a world succumbed to darkness.
+        Accompanied by over-ambitious noble Saffron and the boisterous pirate Cole, the duo set out on a quest to lift Araceli's curse and return home and fulfill her destiny as her kingdom's hero. The party is threatened at every turn by the perpetrators of the destruction: Nox Noctis, a mysterious organization seeking to seize control of a mythical power. Together, these four will put a stop to Nox Noctis' plans, uncover ancient secrets, and bring light to a world succumbed to darkness.
       </p>
       <h1 style="text-align: center">
         Main Characters
@@ -55,7 +55,7 @@
           </div>
           <hr>
           <p style="margin: 5px">
-            Ten years ago, Raiden washed up on the shores of Buragee with no recollection of her identity outside of her own name. Graciously, she was offered refuge with the Cielisono Royal Family and began serving as its princess' attendant - as her "shadow." She is deeply thankful for the family's kindness, repaying them with her unwavering dedication to the sole heiress.
+            Six years ago, Raiden washed up on the shores of Ikoron with no recollection of her identity outside of her own name. Graciously, she was offered refuge with the Cielisono Royal Family and began serving as its princess' attendant—as her "shadow." She is deeply thankful for the family's kindness, repaying them with her unwavering dedication to the sole heiress.
           </p>
           <hr>
           <div style="margin: 5px">
@@ -91,19 +91,19 @@
           </div>
           <hr>
           <p style="margin: 5px">
-            Princess of the Cielisono Royal Family and future ruler of its kingdom, Araceli spent most of her time studying under an Elitecan tutor, training with the Windknights in magic and swordplay, and tending to her pact with Champion Loniles. To the rest of the world, Araceli was the picture-perfect heiress for the kingdom. Behind all of that façade, however, was a young girl cursed to fail her kingdom.
+            Princess of the Cielisono Royal Family and future ruler of its kingdom, Araceli spent most of her time studying under an Arcanican tutor, training with the Windknights in magic and swordplay, and trying to hone her pact with the Champion Loniles. To the rest of the world, Araceli was the picture-perfect heiress for the kingdom. Behind all of that façade, however, was a young girl cursed to fail her kingdom.
           </p>
           <hr>
           <div style="margin: 5px">
             <ul style="margin: 0px">
               <li>
-                Enjoys reading young adult adventure novels
+                Enjoys reading young adult adventuromance novels
               </li>
               <li>
                 Bites her knuckles as a nervous tick
               </li>
               <li>
-                Keeps a <a href="lunar/index" class="secret-link">diary</a> that no one else - not even Raiden - is allowed to peek in
+                Keeps a <a href="lunar/index" class="secret-link">diary</a> that no one else—not even Raiden—is allowed to peek in
               </li>
             </ul>
           </div>
@@ -127,13 +127,13 @@
           </div>
           <hr>
           <p style="margin: 5px">
-            Saffron is the second-born son of House Infiaco, and he is very proud of this fact. Though, as much as he'd like to inherit the role of Head of House, it seems his older brother has seniority. Convinced that he would be far more suited as South Pyrus' leader, Saffron constantly pesters his mother to reconsider. His mother gives him but one response: Saffron just doesn't have what it takes, and until he acquires it, the position will be inherited by his brother.
+            Saffron is the second-born son of House Infiaco, and he is very proud of this fact. Upon warning his family of an impending catastrophe, he realizes that everyone has already long-resolved themselves to this end—it will happen, and trying to change fate could result in divine punishment. Saffron, detached from fate as a member of House Infiaco could be, refuses to take this as an answer, and embarks on a quest to make everyone listen to him.
           </p>
           <hr>
           <div style="margin: 5px">
             <ul style="margin: 0px">
               <li>
-                Despite his envy and annoyance, he actually admires his older brother
+                Despite his envy and annoyance, he actually admires his brothers
               </li>
               <li>
                 Horrible with animals and small children
@@ -145,7 +145,7 @@
           </div>
           <hr>
           <p class="blockquote" style="font-style: italic; margin: 5px 15px">
-            "Excuse me as I question your judgement for a moment—just WHAT were you thinking?! Do you have even a touch of logical reasoning in you? No, of course you don't! Why do I even ask...”
+            "Just WHAT were you thinking?! Do you have even a touch of logical reasoning in you? No, no, of course you don't! I am unsure why I even asked...”
           </p>
         </div>
       </div>
@@ -163,7 +163,7 @@
           </div>
           <hr>
           <p style="margin: 5px">
-            Cole's reputation as the renegade first mate of one of the most notorious bands of sky pirates, The Four Skulls, preceded her; few individuals of any faction were comfortable approaching her for any reason, and she preferred it that way. However, under all of that tough skin, Cole buries a tragic secret. When an unusual group request a means of travel, Cole is forced to face this trauma head-on.
+            Cole's reputation as the renegade first mate of one of the most notorious bands of sky pirates, The Four Skulls, preceded her; few individuals of any faction were comfortable approaching her for any reason, and she preferred it that way. However, under all of that tough skin, Cole buries a tragic secret. When an interesting noble requests a means of travel, Cole is forced to face this trauma head-on.
           </p>
           <hr>
           <div style="margin: 5px">
