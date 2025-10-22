73f2077aa8d8cf0130693ade3317df24f1029b4c
THUNDER <lenicarusdo@gmail.com>
2025-10-21 23:23:20 -0400

    LINK STATES BABYYY
    GIVE IT UP FOR THE FIRST GAMEPLAY PAGE (still keeping that nav link disabled lol)

---
+++ bftb/wiki/game/link.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bolt from the Blue Wiki</title>
+
+<!-- discord embed -->
+    <meta content="the thunder guy or whatever" property="og:site_name">
+    <meta content="Bolt from the Blue - Link State" property="og:title">
+    <meta content="The 'official' wiki for LEN's worldbuilding and RPG project, Bolt from the Blue." property="og:description">
+    <meta content='https://rkrk.dev/bftb/wiki/res/icon.png' property='og:image'>
+    <meta name="theme-color" content="#0036cc">
+
+<!-- css sheet and javascript -->
+    <link href="../style_wiki.css" rel="stylesheet" type="text/css" media="all">
+    <script src="../script_wiki.js"></script>
+
+  </head>
+  <body>
+    <div class="all-content">
+      <?php include '../header_content.html'; ?>
+      <main>
+        <ul class="crumb">
+          <li><a href="../index">Home</a></li>
+          <li><a href="">Gameplay</a></li>
+          <li><a href="link">Link State</a></li>
+        </ul>
+        <h1>
+          Link State
+        </h1>
+        <hr>
+        <p>
+          Link State (stylized as Link State) is a special ability exclusive to Raiden. Each type, known as a Link, is obtained as a reward for completing another character's Link Quest. When active, Raiden's aspect changes, and she receives a unique set of CAST skills to use in battle based on the respective character's own strengths. Additionally, if the character that the Link State originates from is also in the party, that character is then able to use a special combo CAST skill with Raiden.
+        </p>
+        <p>
+          While being able to switch aspects and roles on the fly is powerful, Link State comes with a plethora of conditions and drawbacks. Raiden's Link meter is drained every time Link State is activated or a Link CAST skill is used. Additionally, activating a Link State slightly damages all other characters in the party (except for the character in the "on deck" position).
+        </p>
+        <p>
+          Raiden's Link meter grows when Raiden heals or cures another ally.
+        </p>
+        <p class="notebox">
+          There are a total of 18 Links that can be obtained in <i>Bolt from the Blue.</i> Raiden's, of course, is unlocked from the very start of the game.
+        </p>
+        <hr>
+      </main>
+      <?php include '../footer_content.html'; ?>
+    </div>
+    <div class="side">
+      <a href="../wiki/character?id=raiden">Beating, bleeding heart of Luxos—don't lose your way.</a>
+    </div>
+  </body>
+</html>
