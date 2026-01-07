61bf867883c71b6acd801d2d04f405105e52abbf
THUNDER <lenicarusdo@gmail.com>
2026-01-06 21:05:38 -0500

    now why arent you working.

--- bftb/story.php
+++ bftb/story.php
@@ -29,21 +29,6 @@
         }
         ?>
         
-        <?php if ($id === 'overall'): ?>
-          <img src="res/focus_overall.png" class="index-img">
-        <?php endif; ?>
-        <?php if ($id === 'raiden'): ?>
-          <img src="res/focus_raiden.png" class="index-img">
-        <?php endif; ?>
-        <?php if ($id === 'araceli'): ?>
-          <img src="res/focus_araceli.png" class="index-img">
-        <?php endif; ?>
-        <?php if ($id === 'saffron'): ?>
-          <img src="res/focus_saffron.png" class="index-img">
-        <?php endif; ?>
-        <?php if ($id === 'cole'): ?>
-          <img src="res/focus_cole.png" class="index-img">
-        <?php endif; ?>
 
 
         <div class="landing-container">
