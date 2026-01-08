16eb8447a55e3693fe62dddd6fcde6e49e9a4451
THUNDER <lenicarusdo@gmail.com>
2026-01-07 19:57:22 -0500

    fixed non-story pages

--- bftb/faq.php
+++ bftb/faq.php
@@ -18,34 +18,36 @@
 
   </head>
   <body>
-    <?php include 'header_content.html'; ?>
-    <main class="landing-main">
-      <img src="res/faq.png" class="index-img">
-      <div class="landing-container">
-        <?php include 'nav_content.html'; ?>
-        <div class="landing-textbox">
-          <h1 style="margin-top: 0">
-            Frequently Asked Questions
-          </h1>
-          <b>Q. When is the game getting released? / When is the demo?</b>
-          <p class="blockquote">
-            A. Whenever it is released, lol. BFTB does not have a solid release date currently, as development is not a top priority at the moment. While I AM writing and doing quite a bit of design work, most of my "game dev" energy is going to some smaller projects, such as <a href="https://onegameamonth.itch.io/">OGAM</a>.
-          </p>
-          <b>Q. What engine is BFTB being made in?</b>
-          <p class="blockquote">
-            Godot 4. Really went back and forth on quite a few engines before settling on this one. I just like their style.
-          </p>
-          <b>Q. How can I help?</b>
-          <p class="blockquote">
-            A. I'm not looking for any kind of financial support at the moment, nor am I bringing any new team members on. This is subject to change as the project starts to take shape, but for now just keep me and my darling project in your heart!... and maybe give me a follow on <a href="/abt">some social medias</a> if you'd like.
-          </p>
-          <b>Q. What does "biting / bite your friends and let them bite you back" mean?</b>
-          <p class="blockquote">
-            A. It's somewhat of a lighthearted phrase between me and some friends to say "take care of your loved ones and let them take care of you too" or "let yourself be vulnerable with others just as they let themselves be with you." It originated with an equally lighthearted description of Raiden's Link abilities: imagery of the wolfdragon Fenrir engulfing or "biting off" a piece of someone else. This power ties into her character—she is someone who shows complete, unrelenting empathy to those around her, and in turn she is shaped by the vulnerabilities entrusted with her ("biting others"). Her arc is about the way in which these exchanges serve as a lens for understanding her own identity, and realizing that she, too, is a person who wants to be loved and cared for by others ("letting others bite her back"). Does that make sense? It probably doesn't. Sorry.
-          </p>
+    <div class="body-container">
+      <?php include 'header_content.html'; ?>
+      <main class="landing-main" style="flex-direction: row">
+        <img src="res/faq.png" class="index-img">
+        <div class="landing-container">
+          <?php include 'nav_content.html'; ?>
+          <div class="landing-textbox">
+            <h1 style="margin-top: 0">
+              Frequently Asked Questions
+            </h1>
+            <b>Q. When is the game getting released? / When is the demo?</b>
+            <p class="blockquote">
+              A. Whenever it is released, lol. BFTB does not have a solid release date currently, as development is not a top priority at the moment. While I AM writing and doing quite a bit of design work, most of my "game dev" energy is going to some smaller projects, such as <a href="https://onegameamonth.itch.io/">OGAM</a>.
+            </p>
+            <b>Q. What engine is BFTB being made in?</b>
+            <p class="blockquote">
+              Godot 4. Really went back and forth on quite a few engines before settling on this one. I just like their style.
+            </p>
+            <b>Q. How can I help?</b>
+            <p class="blockquote">
+              A. I'm not looking for any kind of financial support at the moment, nor am I bringing any new team members on. This is subject to change as the project starts to take shape, but for now just keep me and my darling project in your heart!... and maybe give me a follow on <a href="/abt">some social medias</a> if you'd like.
+            </p>
+            <b>Q. What does "biting / bite your friends and let them bite you back" mean?</b>
+            <p class="blockquote">
+              A. It's somewhat of a lighthearted phrase between me and some friends to say "take care of your loved ones and let them take care of you too" or "let yourself be vulnerable with others just as they let themselves be with you." It originated with an equally lighthearted description of Raiden's Link abilities: imagery of the wolfdragon Fenrir engulfing or "biting off" a piece of someone else. This power ties into her character—she is someone who shows complete, unrelenting empathy to those around her, and in turn she is shaped by the vulnerabilities entrusted with her ("biting others"). Her arc is about the way in which these exchanges serve as a lens for understanding her own identity, and realizing that she, too, is a person who wants to be loved and cared for by others ("letting others bite her back"). Does that make sense? It probably doesn't. Sorry.
+            </p>
+          </div>
         </div>
-      </div>
-    </main>
-    <?php include 'footer_content.html'; ?>
+      </main>
+      <?php include 'footer_content.html'; ?>
+    </div>
   </body>
 </html>
--- bftb/index.php
+++ bftb/index.php
@@ -18,29 +18,31 @@
 
   </head>
   <body>
-    <?php include 'header_content.html'; ?>
-    <main class="landing-main">
-      <img src="res/main.png" class="index-img">
-      <div class="landing-container">
-        <?php include 'nav_content.html'; ?>
-        <div class="landing-textbox">
-          <h1 style="margin: 0; font-size: 2.2rem;">
-            Bite your friends!
-          </h1>
-          <span style="color: var(--subheader); font-size: 1.6rem">...and let them bite you back!</span>
-          <p>
-            <i>Bolt from the Blue</i> is a worldbuilding and RPG gamedev project that I (<a href="/abt">LEN</a>) am developing. It centers around Araceli Almador de Luna and her journey to unlock the power to save her kingdom (and the world). The story heavily leans into themes of love as a transitive/reciprocative force, finding one's strength in others/bonds, and about saving a kind world.
-          </p>
-          <p>
-            In December 2020, I started working on <i>Bolt from the Blue</i> with the purpose of making a world with everything I wanted in it. Now, it's my passion project and one of the main sources of comfort and joy in my life.
-          </p>
-          <p>
-            This part of the website serves as a simple overview of what it's all about. If you'd like a more in-depth look at the characters, lore and narrative, please check out <a href="wiki/index">the wiki</a>. Thanks for stopping by!
-          </p>
+    <div class="body-container">
+      <?php include 'header_content.html'; ?>
+      <main class="landing-main" style="flex-direction: row">
+        <img src="res/main.png" class="index-img">
+        <div class="landing-container">
+          <?php include 'nav_content.html'; ?>
+          <div class="landing-textbox">
+            <h1 style="margin: 0; font-size: 2.2rem;">
+              Bite your friends!
+            </h1>
+            <span style="color: var(--subheader); font-size: 1.6rem">...and let them bite you back!</span>
+            <p>
+              <i>Bolt from the Blue</i> is a worldbuilding and RPG gamedev project that I (<a href="/abt">LEN</a>) am developing. It centers around Araceli Almador de Luna and her journey to unlock the power to save her kingdom (and the world). The story heavily leans into themes of love as a transitive/reciprocative force, finding one's strength in others/bonds, and about saving a kind world.
+            </p>
+            <p>
+              In December 2020, I started working on <i>Bolt from the Blue</i> with the purpose of making a world with everything I wanted in it. Now, it's my passion project and one of the main sources of comfort and joy in my life.
+            </p>
+            <p>
+              This part of the website serves as a simple overview of what it's all about. If you'd like a more in-depth look at the characters, lore and narrative, please check out <a href="wiki/index">the wiki</a>. Thanks for stopping by!
+            </p>
+          </div>
         </div>
-      </div>
-    </main>
-    <?php include 'footer_content.html'; ?>
+      </main>
+      <?php include 'footer_content.html'; ?>
+    </div>
   </body>
 
 </html>
