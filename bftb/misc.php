4187d013e9b2328ccebf766878667412b1e92f51
THUNDER <lenicarusdo@gmail.com>
2026-04-15 21:56:11 -0400

    fixed some little things that broke

--- bftb/misc.php
+++ bftb/misc.php
@@ -18,81 +18,83 @@
 
   </head>
   <body>
-    <?php include 'header_content.html'; ?>
-    <main style="background-color: initial; border: initial; box-shadow: initial; border-radius: initial; padding: initial;">
-      <div class="landing-container" style="width: 100%; margin: 0px">
-        <?php include 'nav_content.html'; ?>
-        <div class="landing-textbox" style="margin-right: 0">
-          <h1 style="margin-top: 0">
-            Miscellaneous
-          </h1>
-          <hr>
-          <h2>
-            Extra Content
-          </h2>
-          <ul>
-            <li>
-              <a href="lunchtime">Lunchtime</a> — A special page for the pairing between Araceli and Raiden!
-            </li>
-            <li>
-              <a href="https://toyhou.se/charge/characters/folder:1796863" target="_blank">TOYHOU.SE folder</a> — Little character profiles that you can comment on and enjoy from the comfort of TH
-            </li>
-          </ul>
-          <h2>
-            Inspirations
-          </h2>
-          <p>
-            Behind every "very bad but very good" RPG is an "even worse but even better" RPG.
-          </p>
-          <p>
-            <i>Bolt from the Blue</i> is mainly inspired by Team Asano JRPGs, but here's a "full" list of inspirations:
-          </p>
-          <ul>
-            <li>
-              Bravely series
-            </li>
-            <li>
-              The Legend of Zelda series (esp. BOTW/TOTK)
-            </li>
-            <li>
-              Voice of Cards series (esp. The Forsaken Maiden)
-            </li>
-            <li>
-              Kingdom Hearts
-            </li>
-            <li>
-              Octopath Traveler (esp. Octopath Traveler 2)
-            </li>
-            <li>
-              Genshin Impact (...to a gradually-diminishing extent)
-            </li>
-            <li>
-              Various shounen (Black Clover, Ichi the Witch, Naruto)
-            </li>
-          </ul>
-          <h2>
-            Credits/Acknowledgements
-          </h2>
-          <p>
-            Pretty much all the art and writing on this site is my own, unless I say elsewhere. As for website/writing credits...
-          </p>
-          <ul>
-            <li>
-              squid made the <a href="https://squidknees.neocities.org/themes/tvtropes-preview/">TVTropes template</a>!
-            </li>
-            <li>
-              Marvin over at <a href="https://numbersstory.com/" target="_blank">Numbers Story</a> for advice on database organization and .htaccess stuff...thanks dude!
-            </li>
-            <li>
-              gf for sniffing everything I write and making sure it's decent. included her here because i love her sooo much
-            </li>
-            <li>
-              Part of this site uses the <a href="https://fonts.google.com/specimen/Caveat">Caveat</a> font under the <a href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL">Open Font Licence</a>
-            </li>
-          </ul>
+    <div class="body-container">
+      <?php include 'header_content.html'; ?>
+      <main style="background-color: initial; border: initial; box-shadow: initial; border-radius: initial; padding: initial;">
+        <div style="width: 100%; margin: 0px">
+          <?php include 'nav_content.html'; ?>
+          <div class="landing-textbox" style="margin-left: 0">
+            <h1 style="margin-top: 0">
+              Miscellaneous
+            </h1>
+            <hr>
+            <h2>
+              Extra Content
+            </h2>
+            <ul>
+              <li>
+                <a href="lunchtime">Lunchtime</a> — A special page for the pairing between Araceli and Raiden!
+              </li>
+              <li>
+                <a href="https://toyhou.se/charge/characters/folder:1796863" target="_blank">TOYHOU.SE folder</a> — Little character profiles that you can comment on and enjoy from the comfort of TH
+              </li>
+            </ul>
+            <h2>
+              Inspirations
+            </h2>
+            <p>
+              Behind every "very bad but very good" RPG is an "even worse but even better" RPG.
+            </p>
+            <p>
+              <i>Bolt from the Blue</i> is mainly inspired by Team Asano JRPGs, but here's a "full" list of inspirations:
+            </p>
+            <ul>
+              <li>
+                Bravely series
+              </li>
+              <li>
+                The Legend of Zelda series (esp. BOTW/TOTK)
+              </li>
+              <li>
+                Voice of Cards series (esp. The Forsaken Maiden)
+              </li>
+              <li>
+                Kingdom Hearts
+              </li>
+              <li>
+                Octopath Traveler (esp. Octopath Traveler 2)
+              </li>
+              <li>
+                Genshin Impact (...to a gradually-diminishing extent)
+              </li>
+              <li>
+                Various shounen (Black Clover, Ichi the Witch, Naruto)
+              </li>
+            </ul>
+            <h2>
+              Credits/Acknowledgements
+            </h2>
+            <p>
+              Pretty much all the art and writing on this site is my own, unless I say elsewhere. As for website/writing credits...
+            </p>
+            <ul>
+              <li>
+                squid made the <a href="https://squidknees.neocities.org/themes/tvtropes-preview/">TVTropes template</a>!
+              </li>
+              <li>
+                Marvin over at <a href="https://numbersstory.com/" target="_blank">Numbers Story</a> for advice on database organization and .htaccess stuff...thanks dude!
+              </li>
+              <li>
+                gf for sniffing everything I write and making sure it's decent. included her here because i love her sooo much
+              </li>
+              <li>
+                Part of this site uses the <a href="https://fonts.google.com/specimen/Caveat">Caveat</a> font under the <a href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL">Open Font Licence</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </main>
-    <?php include 'footer_content.html'; ?>
+      </main>
+      <?php include 'footer_content.html'; ?>
+    </div>
   </body>
 </html>
