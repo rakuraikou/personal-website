964424595c7d776a1a2570fb7b47e9e4e88e84fd
THUNDER <lenicarusdo@gmail.com>
2026-06-28 22:55:51 -0400

    updated abt

--- abt.php
+++ abt.php
@@ -24,7 +24,7 @@
       <h2>LEN (in all caps)*</h2>
       <h5>*(or, you know, one of some billion other names)</h5>
       <a href="https://bsky.app/profile/rkrk.dev" target="_blank" style="font-variant:small-caps">bluesky</a> .
-      <a href="https://discordapp.com/users/949944587210862593" target="_blank" style="font-variant:small-caps" title="@bravebearer">discord</a> . 
+      <a href="https://discordapp.com/users/949944587210862593" target="_blank" style="font-variant:small-caps" title="@n.1310">discord</a> . 
       <a href="https://toyhou.se/charge/" target="_blank" style="font-variant:small-caps">toyhou.se</a> . 
       <a href="https://artfight.net/~charge" target="_blank" style="font-variant:small-caps">art fight</a> . 
       <a href="https://rakuraikou.tumblr.com/" target="_blank" style="font-variant:small-caps">tumblr</a>
@@ -35,6 +35,9 @@
           </a>
           <p>
             25 year old asian american lesbian demon thing riding the pipe dream of developing a meaningful video game someday. No preference in pronouns, but these days most people use she/her for me. I have a wonderful girlfriend who does not use social media so I can't link anything... but I promise she exists lol. That whole "all caps" thing in my name is optional, I just think it's funnier that way.
+          </p>
+          <p>
+            By day, I make "fake" numbers go up in a doctor's office. By night, I go to karate training (4th kyu in shotokan, just beginning in uechi ryu), draw my OCs (or <a href="/junkbox/fujimine">Fujimine</a>), eat raw fish, or call my friends. I'm also a part of <a href="https://onegameamonth.itch.io/">OGAM</a> and Cerro Creativo. It's a lot.
           </p>
           <p>
             I love video games! My favorite video game genre is "modern JRPG that's clearly trying to appeal to the classic/golden-age fans," so a lot of my top games (as well as the inspiration for my primary worldbuilding project) will reflect this weird niche. I also really enjoy listening to chorus battles, and have an embarrassingly encyclopedic knowledge on the scene.
@@ -61,7 +64,6 @@
             <ul style="margin:0px;">
               <li>👑 Bravely Default: Flying Fairy</li>
               <li>The Legend of Zelda: BOTW/TOTK</li>
-              <li>Monster Hunter Rise</li>
               <li>👑 Octopath Traveler 2</li>
               <li>Splatoon 3</li>
               <li>👑 Taiko no Tatsujin</li>
@@ -76,13 +78,14 @@
           <div class="interests-bottom">
             <ul style="margin:0px;">
               <li>¿?shimon</li>
-              <li>Ado</li>
-              <li>Eve</li>
+              <li>👑 Azari</li>
+              <li>👑 biz</li>
+              <li>The Flob</li>
               <li>Jin/Kagerou Project</li>
-              <li>👑 Tatsuya Kitani</li>
-              <li>TOPHAMHAT-KYO / FAKE TYPE</li>
-              <li>👑 Rib</li>
+              <li>Tatsuya Kitani</li>
+              <li>Rib</li>
               <li>syudou</li>
+              <li>t+pazolite</li>
               <li>👑 ...and lots of video game OSTs lol</li>
             </ul>
           </div>
@@ -98,7 +101,7 @@
               <li>Mogu mogu</li>
               <li>Pandan flavor (especially tea)</li>
               <li>Phở tái</li>
-              <li>👑 Sushi</li>
+              <li>👑 Sashimi/Poke</li>
             </ul>
           </div>
         </div>
@@ -112,8 +115,9 @@
               <li>👑 Chorus battles</li>
               <li>Entomology</li>
               <li>👑 Game development</li>
-              <li>Mycology</li>
+              <li>👑 Ichi the Witch</li>
               <li>Playing piano</li>
+              <li>Shinobi Undercover</li>
               <li>Skateboarding</li>
               <li>👑 Worldbuilding</li>
             </ul>
